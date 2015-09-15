cfe9e620e693b79b3ddc958ba0c889b968bce1e1
koksrs <artem_tereshko@hotmail.com>
2015-09-15 22:36:06 +0200

    Тип кнопки теперь button. Добавлена проверка валидации формы перед отправкой Ajax запроса

--- js/contact.js
+++ js/contact.js
@@ -1,31 +1,29 @@
-$(function () { 
-	$("#button").click(function() {
-		$("#formMain input,#formMain select,#formMain textarea").jqBootstrapValidation(
-		{
-			preventSubmit: true,
-			submitSuccess: function($form, event) {
-				event.preventDefault(); 
-				var name = $("#formMain input#name").val();  
-				var telephone = $("#formMain input#telephone").val();
-				var email = $("#formMain input#email").val();
-				var vkid = $("#formMain input#vkid").val();
-				var result_id = "messageResult";
-				$.ajax({
-					url:     "./send.php",
-					type:     "POST",
-					dataType: "html",
-					data: jQuery("#"+"formMain").serialize(), 
-					success: function() {
-						document.getElementById(result_id).innerHTML = "<p>Ваши данные успешно переданы. Оставайтесь на связи.</p>";
-						$('#formMain').trigger("reset");
-					},
-					error: function() {
-						document.getElementById(result_id).innerHTML = "<p>Возникла ошибка при отправке формы. Попробуйте еще раз</p>";
-						$('#formMain').trigger("reset");
-					}
-				});
-				return true;
-			}
-		});
-	});
+
+$("#button").click(function () {
+    var fv = $('#formMain').data('formValidation'); // FormValidation instance
+    fv.validate();
+    if (fv.isValid()) { // Проверка валидности формы. Ensure that the validate() method is already called before calling this one.
+        var name = $("#formMain input#name").val();
+        var telephone = $("#formMain input#telephone").val();
+        var email = $("#formMain input#email").val();
+        var vkid = $("#formMain input#vkid").val();
+        var result_id = "messageResult";
+        $.ajax({
+            url: "./send.php",
+            type: "POST",
+            dataType: "html",
+            data: jQuery("#" + "formMain").serialize(),
+            success: function (result) {
+                console.log(result);
+                document.getElementById(result_id).innerHTML = "<p>Ваши данные успешно переданы. Оставайтесь на связи.</p>";
+                $('#formMain').trigger("reset");
+                fv.resetForm();
+            },
+            error: function () {
+                document.getElementById(result_id).innerHTML = "<p>Возникла ошибка при отправке формы. Попробуйте еще раз</p>";
+                $('#formMain').trigger("reset");
+                fv.resetForm();
+            }
+        });
+    }
 });
